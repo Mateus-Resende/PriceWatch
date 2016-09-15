338939d9358442a35f967d98b12f99a99e4f9e24
LukePgr <lucas.docs.mail@gmail.com>
2016-09-14 21:04:22 -0300

    Coleta de links Lojas Americanas

--- app/models/lojas_americanas/links.py
+++ app/models/lojas_americanas/links.py
@@ -12,19 +12,23 @@ class SpiderLojasAmericanas(scrapy.Spider):
   start_urls = ['http://www.americanas.com.br/linha/267868/informatica/notebook']
 
   def parse(self, response):
-    for vitrine in response.css('article.single-product.vitrine230'):#classe do produto
-      link_data = {
-          'link': vitrine.css('form::attr("action")').extract_first(),
-          'name': vitrine.css('a::attr("title")').extract_first()
-        }
-        
-      yield link_data
-
+    for vitrine in response.xpath("//*[@id='vitrine']/article"):#classe do produto
+      link = vitrine.xpath("./div[@itemprop='item']/form/div/a/@href").extract_first()
+      name = vitrine.xpath("./div[@itemprop='item']/form/div/a/@title").extract_first()
+      
+      
+      # essa expressão regular (regex) retorna true se encontrar bateria OU suporte OU break OU ...
+      notebook_validation = "(Notebook|Ultrabook|Lenovo|Asus|Tela|Mac|Netbook|Probook)"
+      
+      # se a validação não passar, escreva no arquivo de erros
+      if re.search(notebook_validation, name, re.IGNORECASE):
+        yield dict(link=link, name=name)
+      
+      # ou então, libere para o output do scrapy
+      else:
+        f.write(json.dumps(link) + json.dumps(name) + '\n')
+      
+      
     link_next = response.css('li a.pure-button.next::attr("href")').extract_first()
     if link_next:
       yield scrapy.Request(link_next)
-
-#Clase do produto
-#<div class="top-area-product">
-#Link do produto
-#<a href="http://www.americanas.com.br/produto/125569401/notebook-positivo-stilo-xri3150-intel-dual-core-4gb-500gb-tela-led-14-linux-cinza-escuro&amp;review=1"
