8ee00c7d93488ddf217f1d7adf399936b264b74f
izaoli <izabella.izabelle@gmail.com>
2016-10-22 00:53:18 -0200

    Add files via upload

---
+++ app/eletro_shopping.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from helpers.http_client import HttpClient
+from helpers.processors import Processors
+from helpers.mongo_client import MongoDB
+from models.eletro_shopping.data_extractor import DataExtractor
+import json
+
+db = MongoDB()
+http = HttpClient()
+links_file = open("models/eletro_shopping/list_links.json")
+
+urls_string = links_file.read()
+links_file.close()
+
+products_urls = json.loads(urls_string)
+
+for product_url in products_urls:
+	response = http.get_request(product_url["link"])
+	data_extractor = DataExtractor(response, product_url["link"])
+	data = data_extractor.parse()
+	#db.insert(data)
