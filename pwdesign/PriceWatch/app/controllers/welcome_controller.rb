5ae95b7742f83b10b0c210092b463381980dcb23
itpzzi <itpzzi@gmail.com>
2016-11-01 14:45:26 -0200

    Mudanças no design

--- pwdesign/PriceWatch/app/controllers/welcome_controller.rb
+++ pwdesign/PriceWatch/app/controllers/welcome_controller.rb
@@ -1,5 +1,23 @@
 class WelcomeController < ApplicationController
   def index
-  	@products = Product.paginate(:per_page => 5, :page => params[:page])
+  	
+  	@page = (params[:page] ||= 1).to_i
+  	@per_page = (params[:per_page] ||= 7).to_i
+
+	@products = Product.all
+  	@products = @products.skip((@page-1) * @per_page).limit(@per_page)
+  
+	@num_pages = num_pages
+
   end
+ 
+	private
+		def num_pages
+			decimal = @products.count.to_f / @per_page.to_f
+			pages = decimal.to_i
+
+			if decimal - pages > 0
+				pages += 1
+			end
+		end
 end
